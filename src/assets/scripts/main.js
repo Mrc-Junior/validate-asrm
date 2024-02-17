b320aa8bf668ebbc56cb9a86e4604c4c0aaa3dc6
Marco Junior <50880274+Mrc-Junior@users.noreply.github.com>
2024-02-16 23:55:16 -0300

    Finish first version project

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -59,9 +59,34 @@ document.addEventListener('DOMContentLoaded', function () {
 
   function setError(input, message) {
     const formControl = input.parentElement;
-    const icon = formControl.querySelectorAll('.icon');
+    const icon = formControl.querySelector('.icon');
     formControl.className = 'form-control error';
     icon.className = 'icon fas fa-times-circle';
     input.placeholder = message;
+  }
+  function setSuccess(input) {
+    const formControl = input.parentElement;
+    const icon = formControl.querySelector('.icon');
+    formControl.className = 'form-control success';
+    icon.className = 'icon fas fa-check-circle';
+  }
+  function isEmail(email) {
+    return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email)
+  }
+  function isPhone(phone) {
+    return /^\+?(\d.*){3,}$/.test(phone);
+  }
+  function showModal() {
+    const modal = document.getElementById('modal-success');
+    modal.style.display = 'block';
+    const closeBtn = document.querySelector('.close-button');
+    closeBtn.onclick = function () {
+      modal.style.display = 'none';
+    };
+    window.onclick = function (event) {
+      if(event.target === modal) {
+        modal.style.display = 'none';
+      }
+    };
   }
 });
